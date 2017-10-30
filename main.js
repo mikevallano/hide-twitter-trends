a8ac99d14d9ae05b5f56e91ab330c1e711afa177
mike vallano <mikevallano@gmail.com>
2017-10-30 18:59:40 -0500

    Also hide live video

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
   $('.trends').hide()
+  $('.LiveVideoHomePageModuleContainer').hide() //also hide live video
 
   $("#hide_trends_button").click(function() {
   });
